6ae57bd59074241bfadcec70bd938e8435c615f6
emblem333 <122144556+emblem333@users.noreply.github.com>
2023-02-02 07:38:19 +0000

    [Finish]2-3

---
+++ greeting.rb
@@ -0,0 +1,6 @@
+def greeting(name)
+  return "#{name}" #この行のnameは、引数で渡すname
+  # "Good morning,#{name}!"
+end
+
+puts greeting(90)#jhonを引数として渡す
---
+++ lesson6-1.rb
@@ -0,0 +1,6 @@
+puts "計算を始めます"
+i = gets
+for j = 0 in i > j
+  puts i
+  
+end
